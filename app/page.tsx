5ac1164dfd72ec0fc44c61026aaea1ada4d31008
mohamedahmed-cloud <81932663+mohamedahmed-cloud@users.noreply.github.com>
2024-04-13 22:39:26 +0200

    configure EUI

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+"use client";
+import { EuiProvider, EuiText } from "@elastic/eui";
+import "@elastic/eui/dist/eui_theme_dark.css";
+import React from "react";
+
 export default function Home() {
 	return (
-		<main>
-			<h1>How are your</h1>
-		</main>
+		<EuiProvider colorMode="dark">
+			<EuiText>
+				<p>Hello World!</p>
+			</EuiText>
+		</EuiProvider>
 	);
 }
